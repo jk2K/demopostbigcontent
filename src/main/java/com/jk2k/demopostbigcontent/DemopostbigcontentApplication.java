05ead1a21b582bb904b9a6332c13d6a810eb2b94
jk2k <4025839+jk2K@users.noreply.github.com>
2020-01-09 16:46:28 +0800

    first commit

---
+++ src/main/java/com/jk2k/demopostbigcontent/DemopostbigcontentApplication.java
@@ -0,0 +1,13 @@
+package com.jk2k.demopostbigcontent;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemopostbigcontentApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemopostbigcontentApplication.class, args);
+    }
+
+}
